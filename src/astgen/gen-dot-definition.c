b32e3f645f05a6ad4dabc5120e75b696e92585fa
Lorian Coltof <loriancoltof@gmail.com>
2017-05-05 12:55:01 +0200

    Added color for root node

--- src/astgen/gen-dot-definition.c
+++ src/astgen/gen-dot-definition.c
@@ -5,13 +5,18 @@
 
 #include "lib/array.h"
 
-static void generate_nodes(struct Node *node, FILE *fp) {
+static void generate_node(struct Node *node, FILE *fp) {
     out("    \"%s\" [label=\"%s", node->id, node->id);
     for (int i = 0; i < array_size(node->attrs); i++) {
         struct Attr *a = (struct Attr *)array_get(node->attrs, i);
         out("|%s", a->id);
     }
-    out("\" shape=\"record\"];\n");
+
+    out("\"");
+    if (node->root) {
+        out(" color=\"darkorchid3\"");
+    }
+    out(" shape=\"record\"];\n");
 }
 
 static void generate_children(struct Node *node, FILE *fp) {
@@ -22,10 +27,18 @@ static void generate_children(struct Node *node, FILE *fp) {
     }
 }
 
-static void generate_nodesets(struct Nodeset *nodeset, FILE *fp) {
+static void generate_nodeset(struct Nodeset *nodeset, FILE *fp) {
+    out("    \"%s\"", nodeset->id);
+
+    if (nodeset->root) {
+        out(" [color=\"darkorchid3\"]");
+    }
+    out(";\n");
+}
+static void generate_nodeset_edges(struct Nodeset *nodeset, FILE *fp) {
     for (int i = 0; i < array_size(nodeset->nodes); ++i) {
         struct Node *node = (struct Node *)array_get(nodeset->nodes, i);
-        out("%s -> %s;\n", nodeset->id, node->id);
+        out("    %s -> %s;\n", nodeset->id, node->id);
     }
 }
 
@@ -33,13 +46,17 @@ void generate_dot_definition(struct Config *config, FILE *fp) {
     out("digraph ast {\n");
 
     for (int i = 0; i < array_size(config->nodes); i++) {
-        generate_nodes(array_get(config->nodes, i), fp);
+        generate_node(array_get(config->nodes, i), fp);
+    }
+
+    for (int i = 0; i < array_size(config->nodesets); i++) {
+        generate_nodeset(array_get(config->nodesets, i), fp);
     }
 
     out("\n");
     out("    edge [color=blue]\n");
     for (int i = 0; i < array_size(config->nodesets); i++) {
-        generate_nodesets(array_get(config->nodesets, i), fp);
+        generate_nodeset_edges(array_get(config->nodesets, i), fp);
     }
 
     out("\n");
